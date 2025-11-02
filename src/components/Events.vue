18fc2b5961e16f500dfe15f57d205bb14e1ab279
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-11-02 02:59:03 -0300

    style: change style components

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="flex flex-col items-center gap-6 py-10 px-3">
-        <h2 class="text-[80px] mb-5">Detalhes do Evento</h2>
+    <section class="flex flex-col items-center gap-6 py-10 px-3 ">
+        <h2 class="text-6xl m-5">Detalhes do Evento</h2>
 
         <CardEvents />
         
@@ -15,7 +15,7 @@ import CardEvents from './CardEvents.vue';
 <style scoped>
 h2 {
     font-family: var(--font-title);
-    background-image: linear-gradient(350deg, rgba(115, 46, 184, 1) 60%, rgba(240, 110, 66, 1) 90%);
+    background-image: linear-gradient(355deg, rgba(115, 46, 184, 1) 30%, rgba(240, 110, 66, 1) 80%);
     -webkit-background-clip: text;
     background-clip: text;
     -webkit-text-fill-color: transparent;

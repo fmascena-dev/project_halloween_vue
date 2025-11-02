e4a1ebcc7b1224cba185daf56285220f793bb6c7
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-11-02 01:40:50 -0300

    feat: add Events  and CardEvents components

---
+++ src/components/CardEvents.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="flex gap-5">
+    <Card
+      class="flex flex-col gap-3 border border-gray-800 bg-[#111111] p-3 items-center justify-center rounded-xl w-[259px] h-[194px]"
+    >
+      <CardHeader>
+        <img src="/calendar.svg" alt="calendar icon" />
+      </CardHeader>
+      <CardDescription>
+        <p class="text-gray-600 text-lg">Data</p>
+      </CardDescription>
+      <CardTitle class="text-2xl font-bold"> 31 de Outubro </CardTitle>
+      <CardFooter class="text-gray-600 text-sm"> Quinta-feira </CardFooter>
+    </Card>
+
+    <Card
+      class="flex flex-col gap-3 border border-gray-800 bg-[#111111] p-3 items-center justify-center rounded-xl w-[259px] h-[194px"
+    >
+      <CardHeader>
+        <img src="/clock.svg" alt="clock icon" />
+      </CardHeader>
+      <CardDescription>
+        <p class="text-gray-600 text-lg">Horário</p>
+      </CardDescription>
+      <CardTitle class="text-2xl font-bold"> 20:00 - 02:00 </CardTitle>
+      <CardFooter class="text-gray-600 text-sm"> 6h de terror </CardFooter>
+    </Card>
+
+    <Card
+      class="flex flex-col gap-3 border border-gray-800 bg-[#111111] p-3 items-center justify-center rounded-xl w-[259px] h-[194px"
+    >
+      <CardHeader>
+        <img src="/map.svg" alt="map icon" />
+      </CardHeader>
+      <CardDescription>
+        <p class="text-gray-600 text-lg">Local</p>
+      </CardDescription>
+      <CardTitle class="text-2xl font-bold"> Cassa Assombrada </CardTitle>
+      <CardFooter class="text-gray-600 text-sm"> Rua do Medo, 666 </CardFooter>
+    </Card>
+
+    <Card
+      class="flex flex-col gap-3 border border-gray-800 bg-[#111111] p-3 items-center justify-center rounded-xl w-[259px] h-[194px"
+    >
+      <CardHeader>
+        <img src="/peoples.svg" alt="peoples icon" />
+      </CardHeader>
+      <CardDescription>
+        <p class="text-gray-600 text-lg">Dress Code</p>
+      </CardDescription>
+      <CardTitle class="text-2xl font-bold"> Fantasias </CardTitle>
+      <CardFooter class="text-gray-600 text-sm"> Obrigatório! </CardFooter>
+    </Card>
+  </section>
+</template>
+
+<script lang="ts">
+import Card from './ui/card/Card.vue'
+import CardDescription from './ui/card/CardDescription.vue'
+import CardFooter from './ui/card/CardFooter.vue'
+import CardHeader from './ui/card/CardHeader.vue'
+import CardTitle from './ui/card/CardTitle.vue'
+</script>
+
+<style></style>
---
+++ src/components/Events.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="flex flex-col items-center gap-6 py-10 px-3">
+        <h2 class="text-[80px] mb-5">Detalhes do Evento</h2>
+
+        <CardEvents />
+        
+    </section>
+</template>
+
+<script lang="ts" setup>
+import CardEvents from './CardEvents.vue';
+
+</script>
+
+<style scoped>
+h2 {
+    font-family: var(--font-title);
+    background-image: linear-gradient(350deg, rgba(115, 46, 184, 1) 60%, rgba(240, 110, 66, 1) 90%);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent;
+  }
+</style>
